1223d2e7fd26433f6f723cb8c5e42aefd5e3edcf
EnzoMShiotuqui <RM551508@fiap.com.br>
2023-03-30 11:35:15 -0300

    Fazendo ex4

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ if(imc < 18.5){
 }else{
     console.log("Acima do peso")
 }
+
+/** Exercicio 4 
+ * Faça um programa que receba idade de uma pessoa e exiba uma mensagem indicando em qual faixa
+ * etária ela se encontra: criança (até 12 anos), adolescente (entre 13 e 18 anos), adulto (entre 19 e60 anos)
+ * ou idoso (mais de 60 anos).Exemplifique e demonstre a saída
+*/
+
+let idade = 25
+
+if(idade <= 12){
+    console.log("Criança")
+}else if(idade >= 13 && idade <= 18){
+    console.log("Adolecente")
+}else if(idade >= 19 && idade <= 60){
+    console.log("Adulto")
+}else{
+    console.log("Idoso")
+}
