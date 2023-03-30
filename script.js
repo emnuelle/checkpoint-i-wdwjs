3cb325b28e352b8c804d98e5ca576200df5ef99a
EnzoMShiotuqui <RM551508@fiap.com.br>
2023-03-30 11:33:59 -0300

    Arrumando o css e fazendo ex2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Exercicio 1 / Como declarar uma variável com valor undefined em JavaScript?Exemplifique e demonstre a saída do valor da variável!
 let num
 console.log(num)
+
+// Exercicio 2 / Como usar o operador % em JavaScript? Exemplifique e demonstre a saída!
+let num1 = 20
+let num2 = 10
+
+resultado = !(num1 % num2)
+
+if(resultado != 0){
+    console.log("O numero é par")
+}else{
+    console.log("O numero é impar")
+}
