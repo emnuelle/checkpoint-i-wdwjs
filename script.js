bfead16cdc9e2e8104b904691b869c1f0bda9699
EnzoMShiotuqui <RM551508@fiap.com.br>
2023-03-30 11:34:33 -0300

    Fazendo ex3

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ if(resultado != 0){
 }else{
     console.log("O numero é impar")
 }
+
+/** Exercicio 3 
+ * Escreva  um  programa  que  calcula  o  índice  de  massa  corporal  (IMC)  de  uma  pessoa  e  exibe  uma
+ * mensagem indicando se ela está abaixo, no peso ideal ou acima do peso. Considere as seguintes faixas de 
+ * IMC:  abaixo  de  18,5  (abaixo  do  peso),  entre18,5  e  24,9  (peso  ideal)  e  acima  de  24,9  (acima  do  peso).
+ * Exemplifique e demonstre a saída!
+*/
+
+let imc = 25
+
+if(imc < 18.5){
+    console.log("abaixo  do  peso")
+}else if(imc >= 18.5 && imc <= 24.9){
+    console.log("peso ideal")
+}else{
+    console.log("Acima do peso")
+}
