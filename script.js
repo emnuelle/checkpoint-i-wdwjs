8941e22de66351eae2448d453ad30fbb9a4280fb
MuriloRoveri <RM97893@fiap.com.br>
2023-03-30 10:58:42 -0300

    Resolvi o Exercício 3.

--- script.js
+++ script.js
@@ -14,7 +14,12 @@ console.log(resultado); // Aparecerá: 1
 
 
 // Exercício 3
-// Cálculo de Índice de Massa Corporal
+/* 
+ * Escreva  um  programa  que  calcula  o  índice  de  massa  corporal  (IMC)  de  uma  pessoa  e  exibe  uma
+ * mensagem indicando se ela está abaixo, no peso ideal ou acima do peso. Considere as seguintes faixas de 
+ * IMC:  abaixo  de  18,5  (abaixo  do  peso),  entre18,5  e  24,9  (peso  ideal)  e  acima  de  24,9  (acima  do  peso).
+ * Exemplifique e demonstre a saída!
+*/
 let imc = 20
 
 if(imc < 18.5) {
