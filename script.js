a4888c13b326165a2bab84618364871e6183567a
emnuelle <rm97973@fiap.com.br>
2023-04-03 18:59:13 -0300

    tentando fazer :/

---
+++ script.js
@@ -0,0 +1,17 @@
+// ex 1 declarando uma variavél no valor undefined
+let num 
+console.log(num)
+// ex 2 usando operador %
+let num1 = 20
+let num2 = 10
+
+resultado =! (num1 % num2)
+
+if(resultado != 0){
+    console.log("É par!")
+}
+else{
+    console.log("É impar!")
+}
+
+// ex 3 
