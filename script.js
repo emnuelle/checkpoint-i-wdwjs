20ad8bbb1afc8af2f84cc8c4fed75ec89011d441
emnuelle <rm97973@fiap.com.br>
2023-04-03 19:08:04 -0300

    adicionando o ex 4

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let num 
 console.log(num)
 // ex 2 usando operador %
-let num1 = 20
-let num2 = 10
+let num1 = 25
+let num2 = 11
 
 resultado =! (num1 % num2)
 
@@ -14,4 +14,34 @@ else{
     console.log("É impar!")
 }
 
-// ex 3 
+// ex 3 calculando imc 
+
+let imc = 20.6
+
+if(imc < 18.5){
+    console.log("Abaixo do peso")
+}
+else if (imc >= 18.5 && imc <= 24.9){
+    console.log("Peso ideal")
+}
+else{
+    console.log("Acima do peso")
+}
+
+// ex 4 indicando a faixa etária
+let idade = 56
+
+if(idade <= 12){
+    console.log("Criança")
+}
+else if(idade >= 13 && idade <=18){
+    console.log("Adolescente")
+}
+else if(idade >= 19 && idade <=60){
+    console.log("Adulto")
+}
+else{
+    console.log("Idoso")
+}
+
+// 
