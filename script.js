4607d90fdf05a8ee6c8667bef7930bcb0b102bb1
paulo626 <paulorfp5@gmail.com>
2023-04-03 01:17:32 -0300

    exercicio 2 com o operador %

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // começando os exercicios,como declara variavel com valor undefined
 let num
-console.log(num)
+console.log(num) 
+
+// Exercicio 2 / Como usar o operador % em JavaScript
+let num1 = 20
+let num2 = 10
+
+resultado = !(num1 % num2)
+
+if(resultado != 0){
+    console.log("O numero é par")
+}else{
+    console.log("O numero é impar")
+}
